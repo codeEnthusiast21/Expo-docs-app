d076aa22356e0d04cb7bec7150804fbf57668cf5
codeEnthusiast21 <aviralagarwal190006@gmail.com>
2025-10-08 13:28:28 +0530

    added select iamge & sticker feature

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,13 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
-import {Image} from 'expo-image';
-import ImageViewer from "@/components/ImageViewer";
-import Button from '@/components/Button'
-import * as ImagePicker from 'expo-image-picker';
-import {useState} from 'react';
-import IconButton from '@/components/IconButton';
+import Button from '@/components/Button';
 import CircleButton from '@/components/CircleButton';
+import EmojiPicker from '@/components/EmojiPicker';
+import IconButton from '@/components/IconButton';
+import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+import { ImageSourcePropType, StyleSheet, View } from "react-native";
+import EmojiList from '@/components/EmojiList';
+
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
@@ -14,57 +15,78 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
 
-  const [selectedImage,setSelectedImage] = useState<string | undefined>(undefined);
-  const [showAppOptions,setShowAppOptions] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
 
-  const pickImageAsync =async()=>{
+
+  const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes:['images'],
-      allowsEditing:true,
-      quality:1,
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
     });
 
-    if(!result.canceled){
+    if (!result.canceled) {
 
       setSelectedImage(result.assets[0].uri);
       setShowAppOptions(true);
-    }else{
+    } else {
       alert('you did not select any image.');
     }
   };
 
-  const onreset =()=>{
+  const onReset = () => {
     setShowAppOptions(false);
   };
-  const onAddSticekr = ()=>{
+  const onAddSticker = () => {
+    setIsModalVisible(true);
 
   };
-  
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {
+
+  };
+
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
-      {showAppOptions?(
-        <View/>
-      ):(
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label = "Choose a foto" onPress={pickImageAsync}/>
-        <Button label = "Use this foto" onPress={()=> setShowAppOptions(true)}/>
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose a foto" onPress={pickImageAsync} />
+          <Button label="Use this foto" onPress={() => setShowAppOptions(true)} />
+        </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
   },
-   imageContainer: {
+  imageContainer: {
     flex: 1,
   },
   image: {
@@ -72,9 +94,17 @@ const styles = StyleSheet.create({
     height: 440,
     borderRadius: 18,
   },
-   footerContainer: {
+  footerContainer: {
     flex: 1 / 4,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 
 });
